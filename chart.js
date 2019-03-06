07c9222e00331eb0c0e1d1871b53dc98ce3eb522
SarahBridges <47224716+SarahBridges@users.noreply.github.com>
2019-03-06 12:16:38 -0500

    Create chart.js

---
+++ chart.js
@@ -0,0 +1,8 @@
+var dataP = d3.csv("API_SE.PRM.UNER_DS2_en_csv_v2_10476963.csv");
+
+var drawChart = function(dataP)
+{
+  var width = 400;
+  var height = 600;
+  var barwidth = width/dataP.length;
+}
