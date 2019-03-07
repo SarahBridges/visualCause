815d9eb3471a7a70fe909356590a639e74eaf9c9
SarahBridges <47224716+SarahBridges@users.noreply.github.com>
2019-03-06 22:25:47 -0500

    Update chart.js

--- chart.js
+++ chart.js
@@ -20,10 +20,10 @@ var drawChart=function(edu)
     {return i*barWidth;})
   .attr("y", function(d)
     {
-     return height-d.Year})
+     return height-d.Year/10000})
   .attr("width", barWidth)
   .attr("height", function(d)
-    {console.log(d);return height - (d.Year/100000)})
+    {console.log(d);return height-d.Year/100000})
   .attr("fill", function(d)
     {return "blue";})
 }
